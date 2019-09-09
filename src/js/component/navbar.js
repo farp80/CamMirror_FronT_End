2da4c74b3627df47e608b2ddb6a8ed7b20a52723
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-09-09 18:32:05 +0000

    added cards/css

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
 export class Navbar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
+
 	render() {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
