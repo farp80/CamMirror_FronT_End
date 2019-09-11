d8533c56f84f28acd935fc1294dd98156ae854a6
Jorge Mair <jorgemair@yahoo.com>
2019-09-11 22:00:47 +0000

    added backgroung img

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -22,46 +22,48 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div className="container">
-					<div className="jumbotron jumbotron-fluid">
-						<div className="container">
-							<h1 className="display-4 text-center">Welcome!</h1>
-							<p className="lead text-center">This is a where perfect selfies are made.</p>
-						</div>
-					</div>
-				</div>
-
-				<div className="container">
-					<div className="row">
-						<div className="col-sm-6">
-							<div className="card">
-								<div className="card-body">
-									<h5 className="card-title">Try For Free</h5>
-									<p className="card-text">
-										Want to see the possibilites? Start your free trial today.
-									</p>
-									<button className="btn btn-primary">
-										<i className="fas fa-camera" />
-									</button>
-								</div>
+				<div className="body">
+					<div className="container">
+						<div className="jumbotron jumbotron-fluid">
+							<div className="container">
+								<h1 className="display-4 text-center">Welcome!</h1>
+								<p className="lead text-center">This is a where perfect selfies are made.</p>
 							</div>
 						</div>
-						<div className="col-sm-6">
-							<div className="card">
-								<div className="card-body">
-									<h5 className="card-title">Contact Us</h5>
-									<p className="card-text">Want to get in touch with us? Cick below.</p>
-									<Button id="Popover1" type="button">
-										Email Us!
-									</Button>
-									<Popover
-										placement="bottom"
-										isOpen={this.state.popoverOpen}
-										target="Popover1"
-										toggle={this.toggle}>
-										<PopoverHeader>Email questions to</PopoverHeader>
-										<PopoverBody>desravines1100@yahoo.com</PopoverBody>
-									</Popover>
+					</div>
+
+					<div className="container">
+						<div className="row">
+							<div className="col-sm-6">
+								<div className="card">
+									<div className="card-body">
+										<h5 className="card-title">Try For Free</h5>
+										<p className="card-text">
+											Want to see the possibilites? Start your free trial today.
+										</p>
+										<button className="btn btn-primary">
+											<i className="fas fa-camera" />
+										</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-sm-6">
+								<div className="card">
+									<div className="card-body">
+										<h5 className="card-title">Contact Us</h5>
+										<p className="card-text">Want to get in touch with us? Cick below.</p>
+										<Button id="Popover1" type="button">
+											Email Us!
+										</Button>
+										<Popover
+											placement="bottom"
+											isOpen={this.state.popoverOpen}
+											target="Popover1"
+											toggle={this.toggle}>
+											<PopoverHeader>Email questions to</PopoverHeader>
+											<PopoverBody>desravines1100@yahoo.com</PopoverBody>
+										</Popover>
+									</div>
 								</div>
 							</div>
 						</div>
