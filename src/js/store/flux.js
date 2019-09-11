0d3bd6b30250f513a3706c92942afe78e79c9098
farp80 <farp80@gmail.com>
2019-09-11 00:06:09 +0000

    context added

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -15,6 +15,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
+			onLogin: (email, password) => console.log(" EMAIL " + email + "PWD " + password),
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
