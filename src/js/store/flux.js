ebd781a1dc82f3c23a7845848b6bbbf0c0060bdd
farp80 <farp80@gmail.com>
2019-09-11 00:57:14 +0000

    context added

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -15,7 +15,28 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			onLogin: (email, password) => console.log(" EMAIL " + email + "PWD " + password),
+			onLogin: (email, password) => {
+				let settings = {
+					email: email,
+					password: password
+				};
+				fetch("https://3000-db0eec2e-6296-42b2-acf9-c6eef2c1afa7.ws-us1.gitpod.io/login", {
+					method: "POST",
+					body: JSON.stringify(settings),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => {
+						return response.json();
+					})
+					.then(data => {
+						console.log("TOKEN: " + data.jwt);
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
