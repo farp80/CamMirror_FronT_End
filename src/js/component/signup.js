9cb6e7f290d312e9b9f82235a6e44037bb8b4e34
farp80 <farp80@gmail.com>
2019-09-13 00:56:08 +0000

    logic for the signup

--- src/js/component/signup.js
+++ src/js/component/signup.js
@@ -4,46 +4,69 @@ import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 
 export class Signup extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			email: "",
+			password: ""
+		};
+	}
+
+	onErrorHandling = error => {
+		if (error !== "") {
+			return <div>{error}</div>;
+		} else {
+			return false;
+		}
+	};
+
 	render() {
 		return (
 			<Context.Consumer>
-				{({ store }) => {
+				{({ store, actions }) => {
 					return (
 						<React.Fragment>
 							<div className="signup">
-								<form>
-									<div className="form-group">
-										<label forHtml="exampleInputEmail1">Email address</label>
-										<input
-											type="email"
-											className="form-control"
-											id="exampleInputEmail1"
-											aria-describedby="emailHelp"
-											placeholder="Enter email"
-										/>
-										<small id="emailHelp" className="form-text text-muted">
-											We will never share your email with anyone else.
-										</small>
-									</div>
-									<div className="form-group">
-										<label forHtml="exampleInputPassword1">Create Password</label>
-										<input
-											type="password"
-											className="form-control"
-											id="exampleInputPassword1"
-											placeholder="Password"
-										/>
-									</div>
-									<div className="form-group form-check">
-										<input type="checkbox" className="form-check-input" id="exampleCheck1" />
-										<label className="form-check-label" forHtml="exampleCheck1">
-											Check me out
-										</label>
-									</div>
-									<button type="submit" className="btn btn-primary">
-										Signup
-									</button>
-								</form>
+								<div className="form-group">
+									<label forHtml="exampleInputEmail1">Email address</label>
+									<input
+										onChange={e => this.setState({ email: e.target.value })}
+										type="email"
+										className="form-control"
+										id="exampleInputEmail1"
+										aria-describedby="emailHelp"
+										placeholder="Enter email"
+									/>
+									<small id="emailHelp" className="form-text text-muted">
+										{"We will never share your email with anyone else."}
+									</small>
+								</div>
+								<div className="form-group">
+									<label forHtml="exampleInputPassword1">{"Create Password"}</label>
+									<input
+										onChange={e => this.setState({ password: e.target.value })}
+										type="password"
+										className="form-control"
+										id="exampleInputPassword1"
+										placeholder="Password"
+									/>
+								</div>
+								<div className="form-group form-check">
+									<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+									<label className="form-check-label" forHtml="exampleCheck1">
+										{"Check me out"}
+									</label>
+								</div>
+								<button
+									type="submit"
+									className="btn btn-primary"
+									onClick={() => actions.onSignup(this.state.email, this.state.password)}>
+									{"Signup"}
+								</button>
+								{this.onErrorHandling(store.errorStatus)}
+								<Link to="/">
+									<a href="#"> {"home"}</a>
+								</Link>
 							</div>
 						</React.Fragment>
 					);
