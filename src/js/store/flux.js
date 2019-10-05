4713219f47e8e67a6a9f7bcaa2f3462e006d92f2
Fidel Rosell <farp80@gmail.com>
2019-10-05 15:44:38 -0400

    comments to keep working in login

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -5,10 +5,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
 			token: null,
 			currentUserId: null,
-<<<<<<< HEAD
-=======
-
->>>>>>> d7ce644c76345e3ed4cfcec7a05b1feebe37e6d0
 			profile: {
 				first_name: null,
 				last_name: null,
@@ -19,17 +15,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 				updatedDate: null,
 				cloudinary_folder: null,
 				cloudinary_url: null,
-<<<<<<< HEAD
 				profile_pic_settings: null,
 				membership_name: null
-=======
-				membership_name: null,
-				card_holder_name: null,
-				card_number: null,
-				card_expiration_date: null,
-				card_cvv: null,
-				profile_pic_settings: null
->>>>>>> d7ce644c76345e3ed4cfcec7a05b1feebe37e6d0
 			}
 		},
 		actions: {
@@ -75,9 +62,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 							return response.json();
 						})
 						.then(data => {
-							console.log(" NEW FN: " + data.first_name);
 							let store = getStore();
 							let profile = store.profile;
+
 							profile.first_name = data.first_name;
 							profile.last_name = data.last_name;
 							profile.email = data.email;
@@ -89,26 +76,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log(" You need to Login first.");
 				}
 			},
-			createMembership: (
-				membership_name,
-				card_holder_name,
-				card_number,
-				card_expiration_month,
-				card_expiration_year,
-				card_cvv,
-				history
-			) => {
+			createMembership: (membership_name, currentUserId, history) => {
 				let store = getStore();
 				let settings = {
 					membership_name: membership_name,
-					card_holder_name: card_holder_name,
-					card_number: card_number,
-					card_expiration_date: card_expiration_month + "/" + card_expiration_year,
-					card_cvv: card_cvv,
 					user_id: store.currentUserId
 				};
-				// console.log("cefev", settings);
-				// console.log("###", membership_name);
+				console.log("cefev", settings);
+				console.log("###", membership_name);
 
 				fetch(backend_url + "/membership", {
 					method: "POST",
@@ -125,16 +100,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 					.then(data => {
 						if (data.msg == "User Already Exists") {
 							setStore({ errorStatus: data.msg });
+							history.push("/profilePic");
 						}
-					});
-				fetch(backend_url + "/membership")
-					.then(resp => resp.json())
-					.then(data => {
-						let store = this.state.store;
-						setStore({ profile: data });
-						history.push("/profilePic");
 					})
-
 					.catch(error => {
 						console.log(error);
 					});
@@ -255,6 +223,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 					.then(async () => {
 						let store = getStore();
+						// TODO Fix this. Do a GET first, check if the profile exist, then add or not.
 						const resp = await fetch(backend_url + "/profile", {
 							method: "POST",
 							body: JSON.stringify({
